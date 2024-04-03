2722db2866f969a0bd1bc763e15c1eed67d4589c
leopaul29 <leopaulmartin.iut@gmail.com>
2024-04-03 03:19:37 +0200

    update contract

--- backend/contracts/Staking.sol
+++ backend/contracts/Staking.sol
@@ -2,6 +2,7 @@
 pragma solidity 0.8.24;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/math/Math.sol";
 /*
@@ -31,6 +32,104 @@ contract USDCToken is ERC20("USDC", "USDC"), Ownable(msg.sender) {
     }
 }
 
+contract CDPStaking is Ownable(msg.sender) {
+    CoupDePousseToken private /*immutable*/ rewardToken;
+    uint256 totalSupply;
+    uint256 public cdpPerShare = 10;
+    uint256 public startBlock;
+
+constructor(address _rewardToken) {
+    rewardToken = CoupDePousseToken(_rewardToken);
+}
+
+    PoolInfo[] public poolInfo;
+    struct PoolInfo {
+        IERC20 token;           // Address of LP token contract.
+        uint256  totalSupply; // balance?     // How many allocation points assigned to this pool. CAKEs to distribute per block.
+        uint256 rewardTokenPerShare;  // Last block number that CAKEs distribution occurs.
+        // uint256 accCakePerShare; // Accumulated CAKEs per share, times 1e12. See below.
+    }
+    function poolLength() external view returns (uint256) {
+        return poolInfo.length;
+    }
+    function createPool(IERC20 _token) external onlyOwner() {
+        // shouldnot create a pool with an existing token
+        // comment faire ? check si la balance de l'address du contrat est > 0
+        // ça veut dire qu'une pool exist ET est déjà rempli
+        //uint256 lastRewardBlock = block.number > startBlock ? block.number : startBlock;
+
+        // required rewardTokenPerShare < cdpPerBlock
+        poolInfo.push(PoolInfo({
+            token: _token,
+            totalSupply:0,
+            rewardTokenPerShare: 100
+        }));
+    }
+    mapping(address => uint256) public rewards;
+
+    struct UserInfo {
+        uint256  lpToken; // total staked token
+        uint256  blockStart; // begining of the staking
+        uint256  rewards; // cumulate reward updated in updateReward function
+    }
+    mapping (uint256 => mapping (address => UserInfo)) public userInfo;
+
+    // check uint256 is really necessary
+    modifier updateReward(uint256 _pid, address _account) {
+        // get count of CDP token for a pool
+        // uint256 rewardPerPool = cdpPerShare / poolInfo.length;
+
+        // get user alloc of the pool
+        // PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][msg.sender];
+        // uint256 alloc = user.lpToken / pool.totalSupply;
+
+        // update nb reward total in rewards mapping
+        user.rewards += 10;
+        _;
+    }
+
+    function stake(uint256 _pid, uint256 _amount) external updateReward(_pid, msg.sender) {
+        require(_amount > 0, "_amount = 0");
+        PoolInfo storage pool = poolInfo[_pid];
+        pool.totalSupply += _amount; // = balance
+        
+        UserInfo storage user = userInfo[_pid][msg.sender];
+        user.lpToken += _amount; // = balance
+        user.blockStart = block.number;
+
+        pool.token.transferFrom(msg.sender, address(this), _amount);
+        //event stake
+    }
+
+    function withdraw(uint256 _pid, uint256 _amount) external updateReward(_pid, msg.sender) {
+        // faire un withdraw all only pour ne pas avoir de residu
+        require(_amount > 0, "amount = 0");
+        PoolInfo storage pool = poolInfo[_pid];
+        pool.totalSupply -= _amount; // = balance
+        
+        UserInfo storage user = userInfo[_pid][msg.sender];
+        user.lpToken -= _amount; // = balance
+        // check if blocknumber < currentblocknumber to setreward
+        if(user.lpToken <= 0) {
+            user.blockStart = 0;
+        }
+
+        pool.token.transfer(msg.sender, _amount);
+        // event witdhraw
+    }
+
+    function getReward() external {
+        uint256 length = poolInfo.length;
+        for(uint256 pid = 0; pid < length; ++pid) {
+            UserInfo memory user = userInfo[pid][msg.sender];
+            rewardToken.mint(msg.sender, user.rewards);
+            user.rewards=0;
+        }
+    }
+
+}
+
 contract PairStaking is Ownable(msg.sender) {
     using Math for uint256;
 
@@ -48,7 +147,6 @@ contract PairStaking is Ownable(msg.sender) {
     mapping(address => uint256) public blockStart;
     mapping(address => uint256) public rewardPerAllocation;
     mapping(address => uint256) public rewards;
-    uint public kLast; // reserve0 * reserve1, as of immediately after the most recent liquidity event
 
     constructor() {
         usdcToken=USDCToken(address(0x95401dc811bb5740090279Ba06cfA8fcF6113778));
@@ -62,7 +160,6 @@ contract PairStaking is Ownable(msg.sender) {
         cdpToken.mint(address(this), _amountCdp);
         totalUSDCSupply += _amountUSDC;
         totalCDPSupply += _amountCdp;
-        kLast = _amountUSDC*_amountCdp;
     }
 
     modifier updateReward(address _account) {
@@ -115,4 +212,108 @@ contract PairStaking is Ownable(msg.sender) {
     function getReward() external {
         
     }
+}
+
+contract CDPStaking2 is Ownable(msg.sender) {
+    CoupDePousseToken private /*immutable*/ rewardToken;
+    uint256 totalSupply;
+
+    constructor(address _rewardToken) {
+        rewardToken = CoupDePousseToken(_rewardToken);
+    }
+
+    PoolInfo[] public poolInfo;
+    struct PoolInfo {
+        IERC20 token;           // Address of LP token contract.
+        // uint256  totalSupply; // balance?     // How many allocation points assigned to this pool. CAKEs to distribute per block.
+        // uint256 rewardTokenPerShare;  // Last block number that CAKEs distribution occurs.
+        // uint256 accCakePerShare; // Accumulated CAKEs per share, times 1e12. See below.
+        uint256 weight;
+    }
+    function poolLength() external view returns (uint256) {
+        return poolInfo.length;
+    }
+    function createPool(IERC20 _token, uint256 _weight) external onlyOwner() {
+    // chekc min max weight ??? require NEVER 0
+
+        // shouldnot create a pool with an existing token
+        // comment faire ? check si la balance de l'address du contrat est > 0
+        // ça veut dire qu'une pool exist ET est déjà rempli
+        //uint256 lastRewardBlock = block.number > startBlock ? block.number : startBlock;
+
+        // required rewardTokenPerShare < cdpPerBlock
+        poolInfo.push(PoolInfo({
+            token: _token,
+            // totalSupply:0,
+            weight: _weight
+            // rewardTokenPerShare: 100
+        }));
+    }
+
+    struct UserInfo {
+        uint256  lpToken; // total staked token
+        uint256  blockStart; // begining of the staking
+        uint256  rewards; // cumulate reward updated in updateReward function
+    }
+    mapping (address => UserInfo) public userInfo;
+uint256 public truc1;
+uint256 public truc2;
+    // check uint256 is really necessary
+    modifier updateReward(uint256 _pid, address _account) {
+        if (totalSupply != 0) {
+            // get count of CDP token for a pool
+            PoolInfo memory pool = poolInfo[_pid];
+            UserInfo storage user = userInfo[msg.sender];
+            uint256 rewardAllocPerBloc = user.lpToken / totalSupply;
+truc1=rewardAllocPerBloc;
+            // get user alloc of the pool
+            // PoolInfo storage pool = poolInfo[_pid];
+            // uint256 alloc = user.lpToken / pool.totalSupply;
+            uint256 nbBlock = block.number - user.blockStart;
+truc2=nbBlock;
+            // update nb reward total in rewards mapping
+            user.rewards += nbBlock * rewardAllocPerBloc;
+        }
+        _;
+    }
+
+    function stake(uint256 _pid, uint256 _amount) external updateReward(_pid, msg.sender) {
+        require(_amount > 0, "_amount = 0");
+        PoolInfo memory pool = poolInfo[_pid];
+        totalSupply += _amount * pool.weight; // = balance contract
+        
+        UserInfo storage user = userInfo[msg.sender];
+        user.lpToken += _amount * pool.weight; // = balance user
+        user.blockStart = block.number;
+
+        pool.token.transferFrom(msg.sender, address(this), _amount);
+        //event stake
+    }
+
+    function withdraw(uint256 _pid, uint256 _amount) external updateReward(_pid, msg.sender) {
+        // faire un withdraw all only pour ne pas avoir de residu
+        require(_amount > 0, "amount = 0");
+        PoolInfo memory pool = poolInfo[_pid];
+        totalSupply -= _amount * pool.weight; // = balance contract
+        
+        UserInfo storage user = userInfo[msg.sender];
+        user.lpToken -= _amount * pool.weight; // = balance user
+        // check if blocknumber < currentblocknumber to setreward
+        if(user.lpToken <= 0) {
+            user.blockStart = 0;
+        }
+
+        pool.token.transfer(msg.sender, _amount);
+        // event witdhraw
+    }
+
+    function getReward() external {
+        uint256 length = poolInfo.length;
+        for(uint256 pid = 0; pid < length; ++pid) {
+            UserInfo memory user = userInfo[msg.sender];
+            rewardToken.mint(msg.sender, user.rewards);
+            user.rewards=0;
+        }
+    }
+
 }
